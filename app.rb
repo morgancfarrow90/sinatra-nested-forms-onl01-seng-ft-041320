4bb9bd712c74f4d0fd09da2eb2fd74bc439992d5
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 12:34:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/' do
+   get '/' do
+      erb :root
+    end
+
+    get '/new' do
       erb :'pirates/new'
     end
 
